20efa642291ecbf2084b2f48b8b65feadd9fbf91
Randomexecutable <randomexecutable@gmail.com>
2017-02-01 03:45:07 +0200

    Initial commit. A simple script that generates truth tables in LaTeX. Uses table.yaml as an input to print the table (see table.yaml for details).

---
+++ ttab.rb
@@ -0,0 +1,43 @@
+require 'yaml'
+
+table = YAML::load_file(File.join(__dir__, 'table.yaml'))
+
+head_align = ""
+header = ""
+table['header'].count.times do |i|
+  if(table['meta']['vertical_bar'] == i + 1)
+    head_align += 'c|'
+  else
+    head_align += 'c'
+  end
+  if(i == 0)
+    header += "$#{table['header'][i]}$"
+  else
+    header += " & $#{table['header'][i]}$"
+  end
+end
+
+data = ""
+
+table['data'].each do |row| 
+  i = 0
+  row.split(" ").each do |num|
+    if(i == 0)
+      data += "  #{num}"
+    else 
+      if(table['meta']['bold'] == i + 1)
+        data += " & \\bf{#{num}}"
+      else
+        data += " & #{num}"
+      end
+    end
+    i += 1
+  end
+  data += " \\\\ \n"
+end
+
+puts "\\begin{tabular}{#{head_align}}"
+puts "  #{header} \\\\"
+puts "  \\midrule"
+puts "#{data}"
+puts "\\end{tabular}"
